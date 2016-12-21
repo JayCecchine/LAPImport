107daa462a71d36ce779c8655878c8bb08c2c2f4
jason.cecchine <jason.cecchine@CALLCENTER704>
2016-12-21 08:49:11 -0500

    Add project files.

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(162, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(148, 58)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(95, 175)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(300, 145)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "Excel Worksheet|*.xlsx|AllFiles|*.*"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 26)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Open File"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(115, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(516, 375)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Public Class Form1
+    'OLEDB for reading .xls/.xlsx
+    Private Const OleSetJet As String = "Microsoft.Jet.OLEDB.4.0"
+    Private Const OleSetAce As String = "Microsoft.ACE.OLEDB.12.0"
+    Private Const OleVer80 As String = "Excel 8.0"
+    Private Const OleVer120 As String = "Excel 12.0 Xml"
+    Private getHeader As String
+    Private oleSet As String
+    Private oleVer As String
+
+    Public PricingSheet As DataSet
+
+
+    Sub Test()
+
+        Dim Filename As String
+        Dim xlsx As New Excel.Application
+        Dim xlsxWorkbooks As Excel.Workbooks = xlsx.Workbooks
+        Dim thisWeekReport As Excel.Workbooks = xlsxWorkbooks.Open(Filename)
+
+
+    End Sub
+    Public Function ReadXls() As DataSet
+        Dim returnData As DataSet = New DataSet
+        Dim result As DialogResult = OpenFileDialog1.ShowDialog
+
+        If result <> DialogResult.OK Then
+            Return Nothing
+        End If
+
+        Label1.Text = OpenFileDialog1.FileName
+        Dim filePath = OpenFileDialog1.FileName
+
+        Try
+            Dim xlsx As New Excel.Application
+            Dim xlsxWorkbooks As Excel.Workbooks = xlsx.Workbooks
+            Dim thisWeekReport As Excel.Workbooks = xlsxWorkbooks.Open(filePath)
+
+            'For each sheet in the workbook
+            'Get the used range 
+            'create blank data table, name it the sheet name
+            'For each column in the used range
+            '   add a column to the data table
+            'For each row in the used range
+            '   create a new data row
+            '   for each column in the used range
+            '       new row[column - 1] = cell [row, column]
+            '   Add new row to table
+            'If the new table has more then 0 rows
+            '   add the table to the data set
+            'If the data set has more then 0 tables
+            '   return the data set, else return nothing
+
+
+
+            'MSDN Documentation?
+
+
+            Return returnData
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Return Nothing
+        End Try
+    End Function 'ReadXls
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Test()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        PricingSheet = ReadXls()
+    End Sub
+End Class
